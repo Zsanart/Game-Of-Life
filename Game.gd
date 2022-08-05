1fa837909f562589df8fc3e84e5f35c72a4fc06f
zsanart <zsanart@inf.elte.hu>
2022-08-05 21:21:09 +0200

    less spaghetti, more speed

--- Game.gd
+++ Game.gd
@@ -24,23 +24,14 @@ func setup():
 			board.set_cell(i,j,0)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func update_cells():
 	var cells_to_update = {}
-	for cell in live_cells.keys():
-		for i in range(cell.x-1,cell.x+2):
-			for j in range(cell.y-1,cell.y+2):
-				cells_to_update[Vector2(i,j)] = 0		
-	#print(cells_to_update)			
-	#print(live_cells)			
-	for cell in cells_to_update.keys():
-		#update_cell(cell)
-		var cell_status = board.get_cell(cell.x, cell.y)
-		var new_status = next_status(cell)
-		cells_to_update[cell] = new_status
+	for i in range(width):
+		for j in range(height):
+			var cell_status = board.get_cell(i,j)
+			var cell = Vector2(i,j)
+			var new_status = next_status(cell)
+			cells_to_update[cell] = new_status
 	for cell in cells_to_update:
 		board.set_cell(cell.x, cell.y,cells_to_update[cell])
 		if cells_to_update[cell] == 1:
@@ -48,16 +39,6 @@ func update_cells():
 			live_cells[cell] = 1
 		else:
 			live_cells.erase(cell)
-		#print("new: ",new_status)
-#		if cell_status != new_status:
-#			board.set_cell(cell.x, cell.y,new_status)
-#			if new_status == 1:
-#				print(cell)
-#				live_cells[cell] = 1
-#			else:
-#				live_cells.erase(cell)
-#	for cell in updated_tiles:
-#		board.set_cell(cell.x, cell.y,updated_tiles[cell])	
 
 
 func next_status(cell):
@@ -70,13 +51,6 @@ func next_status(cell):
 	else:
 		return cell_status
 
-#func update_cell(cell):
-#	var cell_status = board.get_cell(cell.x, cell.y) # 0 dead, 1 live
-#	var live_neighbors = live_neighbors(cell)						
-#	if cell_status == 1 && (live_neighbors < 2 || live_neighbors > 3):
-#		updated_tiles[cell] = 0
-#	elif cell_status == 0 && live_neighbors == 3:
-#		updated_tiles[cell] = 1
 
 func live_neighbors(cell):
 	var cell_status = board.get_cell(cell.x, cell.y)
@@ -105,17 +79,11 @@ func _unhandled_input(event):
 		if dragging:
 			handle_input_logic(event)	
 
-#func _process(delta):
-#	if Input.is_action_pressed("mouse_down"):
-#		var pos = get_viewport().get_mouse_position()
-#		var tile_pos = board.world_to_map(pos)
-#		handle_input_logic(tile_pos)
 			
 func handle_input_logic(event):
 	var pos = event.position
 	var tile_pos = board.world_to_map(pos)
-	#if event.pressed:
-		#print(tile_pos, ", ",live_neighbors(tile_pos))
+
 			
 	var tile_status = board.get_cell(tile_pos.x, tile_pos.y)
 	if tile_status == 0:
@@ -125,16 +93,6 @@ func handle_input_logic(event):
 		board.set_cell(tile_pos.x, tile_pos.y,0)
 		live_cells.erase(tile_pos)
 						
-		
-#func update_tile(tile_pos):
-#	var live_neighbors = 0
-#	for i in range(tile_pos.x-1,tile_pos.x+1):
-#		for j in range(tile_pos.y-1,tile_pos.y+1):
-#			if board.get_cell(i,j) == 1:
-#				live_neighbors += 1
-#	if live_neighbors < 3 && board.get_cell(tile_pos) == 1:
-#		updated_tiles[tile_pos] = 0
-#	elif live_neighbors 
 
 
 func _on_Timer_timeout():
